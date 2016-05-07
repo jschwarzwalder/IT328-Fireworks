1edd7f36112eaf4b4fb2375ca24cb8bec0c35548
Jami <jamischwarzwalder@gmail.com>
2016-05-06 21:25:59 -0700

    Creating a collection for deck and discards

---
+++ project/collection/collection.js
@@ -0,0 +1,163 @@
+//import the mongo object
+import { Mongo} from "meteor/mongo";
+
+//export access to my collections
+export const deck = new Mongo.Collection("deck");
+
+//export access to any data to start our application
+export const deck = [
+ {
+	 cardValue : 1, 
+	 cardColor: "Blue",
+	 image: "Blue 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Blue",
+	 image: "Blue 1.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Blue",
+	 image: "Blue 2.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Blue",
+	 image: "Blue 3.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Blue",
+	 image: "Blue 4.png"
+ },
+ {
+	 cardValue : 5, 
+	 cardColor: "Blue",
+	 image: "Blue 5.png"
+ },
+  {
+	 cardValue : 1, 
+	 cardColor: "Black",
+	 image: "Black 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Black",
+	 image: "Black 1.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Black",
+	 image: "Black 2.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Black",
+	 image: "Black 2.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Black",
+	 image: "Black 3.png"
+ },{
+	 cardValue : 3, 
+	 cardColor: "Black",
+	 image: "Black 3.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Black",
+	 image: "Black 4.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Black",
+	 image: "Black 4.png"
+ },
+ {
+	 cardValue : 5, 
+	 cardColor: "Black",
+	 image: "Black 5.png"
+ }
+ ];
+ 
+export const discards = [
+
+ {
+	 cardValue : 1, 
+	 cardColor: "Yellow",
+	 image: "Yellow 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Yellow",
+	 image: "Yellow 1.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Yellow",
+	 image: "Yellow 2.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Yellow",
+	 image: "Yellow 3.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Yellow",
+	 image: "Yellow 4.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Yellow",
+	 image: "Yellow 4.png"
+ },
+ {
+	 cardValue : 5, 
+	 cardColor: "Yellow",
+	 image: "Yellow 5.png"
+ },
+  {
+	 cardValue : 1, 
+	 cardColor: "Red",
+	 image: "Red 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Red",
+	 image: "Red 1.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Red",
+	 image: "Red 2.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Red",
+	 image: "Red 3.png"
+ },{
+	 cardValue : 3, 
+	 cardColor: "Red",
+	 image: "Red 3.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Red",
+	 image: "Red 4.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Red",
+	 image: "Red 4.png"
+ },
+ {
+	 cardValue : 5, 
+	 cardColor: "Red",
+	 image: "Red 5.png"
+ }
+ ];
+ 
+ 
