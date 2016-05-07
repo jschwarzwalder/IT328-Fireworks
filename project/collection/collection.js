3a852f902d6f2c543cf3ae97cadd9861584651b6
Jami <jamischwarzwalder@gmail.com>
2016-05-07 16:35:18 -0700

    Full deck is in collection.

--- project/collection/collection.js
+++ project/collection/collection.js
@@ -8,7 +8,20 @@ export const player2HandCollection = new Mongo.Collection("player2Hand");
 export const player_areaCollection = new Mongo.Collection("player_area");
 export const discardCollection = new Mongo.Collection("discard");
 //export access to any data to start our application
+
+//all the cards that are needed in a game of Hanabi
 export const deck = [
+
+ {
+	 cardValue : 1, 
+	 cardColor: "Blue",
+	 image: "Blue 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Blue",
+	 image: "Blue 1.png"
+ },
  {
 	 cardValue : 1, 
 	 cardColor: "Blue",
@@ -20,9 +33,24 @@ export const deck = [
 	 image: "Blue 2.png"
  },
  {
+	 cardValue : 2, 
+	 cardColor: "Blue",
+	 image: "Blue 2.png"
+ },
+ {
 	 cardValue : 3, 
 	 cardColor: "Blue",
 	 image: "Blue 3.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Blue",
+	 image: "Blue 3.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Blue",
+	 image: "Blue 4.png"
  },
  {
 	 cardValue : 4, 
@@ -36,41 +64,34 @@ export const deck = [
  },
  {
 	 cardValue : 1, 
-	 cardColor: "Green",
-	 image: "Green 1.png"
+	 cardColor: "Black",
+	 image: "Black 1.png"
  },
  {
-	 cardValue : 2, 
-	 cardColor: "Green",
-	 image: "Green 2.png"
- },
- {
-	 cardValue : 3, 
-	 cardColor: "Green",
-	 image: "Green 3.png"
- },
- {
-	 cardValue : 4, 
-	 cardColor: "Green",
-	 image: "Green 4.png"
- },
- {
-	 cardValue : 5, 
-	 cardColor: "Green",
-	 image: "Green 5.png"
- },
-  {
 	 cardValue : 1, 
 	 cardColor: "Black",
 	 image: "Black 1.png"
  },
-
+ {
+	 cardValue : 1, 
+	 cardColor: "Black",
+	 image: "Black 1.png"
+ },
  {
 	 cardValue : 2, 
 	 cardColor: "Black",
 	 image: "Black 2.png"
  },
- 
+ {
+	 cardValue : 2, 
+	 cardColor: "Black",
+	 image: "Black 2.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Black",
+	 image: "Black 3.png"
+ },
  {
 	 cardValue : 3, 
 	 cardColor: "Black",
@@ -87,6 +108,21 @@ export const deck = [
 	 image: "Black 4.png"
  },
  {
+	 cardValue : 5, 
+	 cardColor: "Black",
+	 image: "Black 5.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Yellow",
+	 image: "Yellow 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Yellow",
+	 image: "Yellow 1.png"
+ },
+ {
 	 cardValue : 1, 
 	 cardColor: "Yellow",
 	 image: "Yellow 1.png"
@@ -97,9 +133,24 @@ export const deck = [
 	 image: "Yellow 2.png"
  },
  {
+	 cardValue : 2, 
+	 cardColor: "Yellow",
+	 image: "Yellow 2.png"
+ },
+ {
 	 cardValue : 3, 
 	 cardColor: "Yellow",
 	 image: "Yellow 3.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Yellow",
+	 image: "Yellow 3.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Yellow",
+	 image: "Yellow 4.png"
  },
  {
 	 cardValue : 4, 
@@ -117,9 +168,29 @@ export const deck = [
 	 image: "Red 1.png"
  },
  {
+	 cardValue : 1, 
+	 cardColor: "Red",
+	 image: "Red 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Red",
+	 image: "Red 1.png"
+ },
+ {
 	 cardValue : 2, 
 	 cardColor: "Red",
 	 image: "Red 2.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Red",
+	 image: "Red 2.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Red",
+	 image: "Red 3.png"
  },
  {
 	 cardValue : 3, 
@@ -132,10 +203,67 @@ export const deck = [
 	 image: "Red 4.png"
  },
  {
+	 cardValue : 4, 
+	 cardColor: "Red",
+	 image: "Red 4.png"
+ },
+ {
 	 cardValue : 5, 
 	 cardColor: "Red",
 	 image: "Red 5.png"
+ },
+
+ {
+	 cardValue : 1, 
+	 cardColor: "Green",
+	 image: "Green 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Green",
+	 image: "Green 1.png"
+ },
+ {
+	 cardValue : 1, 
+	 cardColor: "Green",
+	 image: "Green 1.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Green",
+	 image: "Green 2.png"
+ },
+ {
+	 cardValue : 2, 
+	 cardColor: "Green",
+	 image: "Green 2.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Green",
+	 image: "Green 3.png"
+ },
+ {
+	 cardValue : 3, 
+	 cardColor: "Green",
+	 image: "Green 3.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Green",
+	 image: "Green 4.png"
+ },
+ {
+	 cardValue : 4, 
+	 cardColor: "Green",
+	 image: "Green 4.png"
+ },
+ {
+	 cardValue : 5, 
+	 cardColor: "Green",
+	 image: "Green 5.png"
  }
+
  ];
  
 export const discards = [
@@ -216,81 +344,4 @@ export const discards = [
  }
  ];
  
-export const play_area = [
-	{
-		 cardValue : 2, 
-		 cardColor: "Red",
-		 image: "Red 2.png"
-	 }
-	,
-	 {
-		 cardValue : 3, 
-		 cardColor: "Yellow",
-		 image: "Yellow 3.png"
-	 }
-	,
-	{
-		 cardValue : 1, 
-		 cardColor: "Black",
-		 image: "Black 1.png"
-	 }
-	 ,
-	 {
-		 cardValue : 4, 
-		 cardColor: "Blue",
-		 image: "Blue 4.png"
-	 }
 
-];
-
-export const player_1 = [{
-	 cardValue : 1, 
-	 cardColor: "Blue",
-	 image: "Blue 1.png"
- },
- {
-	 cardValue : 1, 
-	 cardColor: "Blue",
-	 image: "Blue 1.png"
- },
- {
-	 cardValue : 2, 
-	 cardColor: "Blue",
-	 image: "Blue 2.png"
- },
- {
-	 cardValue : 3, 
-	 cardColor: "Blue",
-	 image: "Blue 3.png"
- },
- {
-	 cardValue : 4, 
-	 cardColor: "Blue",
-	 image: "Blue 4.png"
- }];
-
-export const player_2 = [{
-	 cardValue : 1, 
-	 cardColor: "Blue",
-	 image: "Blue 1.png"
- },
- {
-	 cardValue : 1, 
-	 cardColor: "Blue",
-	 image: "Blue 1.png"
- },
- {
-	 cardValue : 2, 
-	 cardColor: "Blue",
-	 image: "Blue 2.png"
- },
- {
-	 cardValue : 3, 
-	 cardColor: "Blue",
-	 image: "Blue 3.png"
- },
- {
-	 cardValue : 4, 
-	 cardColor: "Blue",
-	 image: "Blue 4.png"
- }];
