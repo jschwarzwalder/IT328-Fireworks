82c5c1c669541f462674ea0923541f7c30c1163c
Sahba Bahizad <sarahjune919@gmail.com>
2016-05-06 23:13:54 -0700

    Waiting to randomely add to firework Collection

--- project/server/main.js
+++ project/server/main.js
@@ -1,21 +1,42 @@
 import { Meteor } from 'meteor/meteor';
 import { deck } from '../collection/collection.js';
+import { fireworkCards } from '../collection/collection.js';
 import { player1HandCollection } from '../collection/collection.js';
 import { player2HandCollection } from '../collection/collection.js';
 //server
 Meteor.startup(() => {
+    
+    //create the deck
+    //remove any database values that are present
+  fireworkCards.remove({}); //delete all records ( this will only work on server side)
+  //add my dummy values
+  for (var i = 0 ; i < deck.length ; i ++) {
+    fireworkCards.insert(deck[i]);
+  }
+  
+  
     var HandNum = 5;
+   //clear hands
        player1HandCollection.remove({});
        player2HandCollection.remove({});
-         for (var i = 0 ; i < HandNum ; i ++) {
+
+   //draw function
+   function drawCard(cardCount, handName) {
+    for (var i = 0 ; i < cardCount ; i ++) {
             
-           // var randomCard = deck.findOne();
-            player1HandCollection.insert(deck[i]);
-            deck.splice(i, 1);
-            player2HandCollection.insert(deck[i]);
-            deck.splice(i, 1);
+           //get a random card from the fireworks card
+         //  var r = Math.floor(Math.random() * );
+          // var randomElement = db.myCollection.find().limit(1).skip(r);
+           //add it to hand
+           //remove it from fireworks
+            handName.insert(deck[Math.round(Math.random() * deck.length) ]);
+            //remove it from the deck collection
+            fireworkCards.findOne.remove();
 
             }
+   }
+   
+     
          
          
 });
