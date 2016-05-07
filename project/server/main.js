550222fb68090c062e8ae47c2b8c2aca1edd99c7
Sahba Bahizad <sarahjune919@gmail.com>
2016-05-06 22:53:47 -0700

    showing 5 cards of each player form deck

--- project/server/main.js
+++ project/server/main.js
@@ -5,20 +5,17 @@ import { player2HandCollection } from '../collection/collection.js';
 //server
 Meteor.startup(() => {
     var HandNum = 5;
-       
+       player1HandCollection.remove({});
+       player2HandCollection.remove({});
          for (var i = 0 ; i < HandNum ; i ++) {
             
            // var randomCard = deck.findOne();
             player1HandCollection.insert(deck[i]);
             deck.splice(i, 1);
+            player2HandCollection.insert(deck[i]);
+            deck.splice(i, 1);
 
             }
          
-         for (var i = 0 ; i < HandNum ; i ++) {
-            
-           // var randomCard = deck.findOne();
-            player2HandCollection.insert(deck[i]);
-            deck.splice(i, 1);
-         }
-
+         
 });
