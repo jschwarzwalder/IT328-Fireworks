09daaf8e7c9618ea18130dee00349b68819fed08
Jami <jamischwarzwalder@gmail.com>
2016-05-08 16:39:29 -0700

    writing psuedocode for what the functions should do

--- project/client/main.js
+++ project/client/main.js
@@ -66,22 +66,54 @@ Template.Counters.helpers({
 Template.playerActions.events({
   'click #play': function(event, template) {
 		//play_areaCollection.find({cardColor: this.cardColor})
+	//set state to play card
+	//listen for a card click
+	//if card is clicked then run play fuction as described below
 
-	 var errors = Session.get('errors');
-	  if(errors < 3){
-	  errors ++;
-	  console.log(errors)
-	  Session.set("errors", errors);
-  }else{
+	var errors = Session.get('errors');
+	if(errors < 3){
+		//find that card color in play area
+		//if color does not exist
+			//check to see if this card has value of 1
+				//if it does add that card to the play area
+				//remove from players hand
+		//if color exists, compare card values
+			//if this card value is play_area card value +1 
+			//remove play_area card 
+			//add this card to the play area
+			//remove this card from players hand
+		
+		//else add to discard 
+			//remove from players hand
+			//increase fireworks (errors)
+			errors ++;
+			console.log(errors)
+			Session.set("errors", errors);
+			
+		//draw a new card	
+		
+	}else{
 	  console.log("Game Over!");
-  }
+	}
   }, 
   'click #cluenum': function(event, template) {
 	  var clues = Session.get('clues');
 	  if(clues <= 8 && clues >0 ){
-	  clues --;
-	  console.log(clues);
-	  Session.set("clues", clues);
+		//set state to clue number 
+		//listen for a card click
+		//if card is clicked then run cluenum fuction as described below
+		
+		//get card value
+		//find all card objects with that value
+		//highlight
+		//alert (ask player if they want to clue these cards)
+		//if yes then change on server and
+		  clues --;
+		  console.log(clues);
+		  Session.set("clues", clues);
+		  
+		//draw a new card	
+		
   }else{
 	  console.log("Do Nothing... Cannot have negative clues!");
   }
@@ -89,19 +121,43 @@ Template.playerActions.events({
   'click #cluecolor': function(event, template) {
 	  var clues = Session.get('clues');
 	  if(clues <= 8 && clues >0 ){
-	  clues --;
-	  console.log(clues);
-	  Session.set("clues", clues);
+		//set state to clue color
+		//listen for a card click
+		//if card is clicked then run cluecolor fuction as described below
+		
+		//get card color
+		//find all card objects with that color
+		//highlight
+		//alert (ask player if they want to clue these cards)
+		//if yes then change on server and
+		  clues --;
+		  console.log(clues);
+		  Session.set("clues", clues);
+		  
+		//draw a new card	
+		
   }else{
 	  console.log("Do Nothing... Cannot have negative clues!");
   }
   }, 
   'click #discard': function(event, template) {
 	  var clues = Session.get('clues');
-	  if(clues < 8 && clues > 0 ){
-	  clues ++;
-	  console.log(clues);
-	  Session.set("clues", clues);
+	  if(clues < 8 && clues >= 0 ){
+		//set state to discards
+		//listen for a card click
+		//if card is clicked then run discard fuction as described below
+		
+		//add card from hand to discards 
+		//sort discards
+		//remove card from hand
+		//update clues
+		
+		  clues ++;
+		  console.log(clues);
+		  Session.set("clues", clues);
+	  
+	  //draw a new card	
+	  
 	  }else{
 		  console.log("Do Nothing... Cannot have negative clues!");
 	  }
@@ -145,12 +201,12 @@ Template.newGame.events({
 	 //shuffle deck
 	 
 	 //deal out hand
-	for (i = 0; i< 5; i++){
+	//for (i = 0; i< 5; i++){
 		
 
 	//console.log(Meteor.call('newGame' , 5, fireworkCards, player1HandCollection, player2HandCollection));
 	Meteor.call('startNewGame');
 
-	  
+	} 
 });
 
