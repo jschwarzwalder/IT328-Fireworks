2759f8f77339caae88fdc3cadd039a0d133b2f25
Sahba Bahizad <sarahjune919@gmail.com>
2016-06-06 20:32:06 -0700

    start button is appearing only after sign in

--- project/routing/routes.js
+++ project/routing/routes.js
@@ -28,7 +28,7 @@ Router.onBeforeAction(function () {
 
   if (!Meteor.userId()) {
     // if the user is not logged in, render the Login template
-    this.render('Login');
+    this.render('login');
   } else {
     // otherwise don't hold up the rest of hooks or our route/action function
     // from running
